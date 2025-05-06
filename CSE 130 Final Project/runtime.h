01cbb01fee2a9fb972ce1b9a92da81b1c42bff9f
thomasHermJr <tjherm01@louisville.edu>
2025-05-06 19:04:07 -0400

    Add files via upload

---
+++ CSE 130 Final Project/runtime.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "menu.h"
+#include "class.h"
+#include "SEC.h"
+
+int construct(int input) { // start runtime funtion
+	
+	Patient patient(input);
+
+	return safeExit;
+	
+} // end function
